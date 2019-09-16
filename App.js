aba1df40ae90b4012f1b1099771292867a306666
Unknown <qhespqhesp@gmail.com>
2019-09-16 18:01:29 +0800

    update play and stream

--- App.js
+++ App.js
@@ -7,7 +7,10 @@
  */
 
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+
+import {  NodeCameraView } from 'react-native-nodemediaclient';
+import {  NodePlayerView } from 'react-native-nodemediaclient';
 
 const instructions = Platform.select({
   ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
@@ -16,14 +19,49 @@ const instructions = Platform.select({
     'Shake or press menu button for dev menu',
 });
 
-type Props = {};
-export default class App extends Component<Props> {
+export default class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isPublish: false,
+      publishBtnTitle: 'test'
+    }
+  }
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
+        <NodeCameraView 
+          style={{ height: 400 }}
+          ref={(vb) => { this.vb = vb }}
+          outputUrl = {"rtmp://192.168.0.10/live/stream"}
+          camera={{ cameraId: 1, cameraFrontMirror: true }}
+          audio={{ bitrate: 32000, profile: 1, samplerate: 44100 }}
+          video={{ preset: 12, bitrate: 400000, profile: 1, fps: 15, videoFrontMirror: false }}
+          autopreview={true}
+        />
+        <Button
+          onPress={() => {
+            if (this.state.isPublish) {
+              this.setState({ publishBtnTitle: 'Start Publish', isPublish: false });
+              this.vb.stop();
+            } else {
+              this.setState({ publishBtnTitle: 'Stop Publish', isPublish: true });
+              this.vb.start();
+            }
+          }}
+          title={this.state.publishBtnTitle}
+          color="#841584"
+        />
+        {/* <NodePlayerView 
+          style={{ height: 200 }}
+          ref={(vp) => { this.vp = vp }}
+          inputUrl={"rtmp://192.168.0.10/live/stream"}
+          scaleMode={"ScaleAspectFit"}
+          bufferTime={300}
+          maxBufferTime={1000}
+          autoplay={true}
+        /> */}
       </View>
     );
   }
